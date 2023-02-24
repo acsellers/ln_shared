bf714a5f15e0e546daa49b6f5a2494d4f3384916
Andrew Sellers <andrew@andrewcsellers.com>
2023-02-24 13:11:08 -0600

    Add publisherslist to shared

---
+++ publishers/publishers.go
@@ -0,0 +1,12 @@
+package publishers
+
+var PublisherFiles = []string{
+	"cross-infinite/output.json",
+	"jnovel/output.json",
+	"kodansha/output.json",
+	"one-peace/output.json",
+	"sevenseas/output.json",
+	"square-enix/output.json",
+	"tentai/output.json",
+	"yenpress/output.json",
+}
